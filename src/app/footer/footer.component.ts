cc0e52b81e9bbe5fb74bd9ec2a8b22fe955e993b
Weronika Kot <97183851+werq183@users.noreply.github.com>
2024-06-16 19:48:18 +0200

    add footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MdbRippleModule} from "mdb-angular-ui-kit/ripple";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MdbRippleModule,
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
