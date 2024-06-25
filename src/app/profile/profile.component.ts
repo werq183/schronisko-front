e0c083efcce96f6d2698a106782d33375879b8b5
Weronika Kot <97183851+werq183@users.noreply.github.com>
2024-06-25 19:28:45 +0200

    Update profile appearance

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -16,6 +16,8 @@ import {NgIf} from "@angular/common";
 export class ProfileComponent implements OnInit {
   profile: any = {};
   error: string | null = null;
+  successMessage: string | null = null;
+
 
   constructor(private profileService: ProfileService) {}
 
@@ -47,12 +49,17 @@ export class ProfileComponent implements OnInit {
 
     this.profileService.updateProfile(updateData).subscribe({
       next: (data) => {
-        this.profile = data;
+        this.profile = data; // Przypisanie zaktualizowanych danych do profilu
         this.error = null;
+        this.successMessage = 'Profil zaktualizowany pomyślnie.';
+        setTimeout(() => this.successMessage = null, 3000);
+        this.loadProfile(); // Ponowne wczytanie profilu
       },
       error: (err) => {
         this.error = err.error.detail || 'Failed to update profile';
+        setTimeout(() => this.error = null, 3000);
       }
     });
   }
+
 }
