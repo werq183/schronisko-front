960caf8ca637b53891ab116f4099d179bcad65c5
Weronika Kot <97183851+werq183@users.noreply.github.com>
2024-06-25 13:46:08 +0200

    Add ogloszenia-detail

--- src/app/ogloszenia-detail/ogloszenia-detail.component.ts
+++ src/app/ogloszenia-detail/ogloszenia-detail.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OgloszeniaService } from '../ogloszenia.service';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-ogloszenia-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './ogloszenia-detail.component.html',
-  styleUrl: './ogloszenia-detail.component.scss'
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  styleUrls: ['./ogloszenia-detail.component.scss']
 })
-export class OgloszeniaDetailComponent {
+export class OgloszeniaDetailComponent implements OnInit {
+  ogloszenie: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private ogloszeniaService: OgloszeniaService
+  ) {}
+
+  ngOnInit(): void {
+    this.getOgloszenie();
+  }
+
+  getOgloszenie(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.ogloszeniaService.getOgloszenieById(id).subscribe(ogloszenie => {
+      this.ogloszenie = ogloszenie;
+    });
+  }
+
+  reserve(): void {
+    // Tutaj zostanie dodana logika rezerwacji
+    console.log('Rezerwacja:', this.ogloszenie.id);
+  }
 }
